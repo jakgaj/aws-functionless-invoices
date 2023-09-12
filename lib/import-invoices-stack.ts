46c3acf2a3cdc7263a138ebd880e2cc3c3bce581
Jakub Gaj <jakub.gaj@lego.com>
2023-09-12 15:21:31 +0200

    Small fixes

--- lib/import-invoices-stack.ts
+++ lib/import-invoices-stack.ts
@@ -42,19 +42,24 @@ export class ImportInvoicesStack extends cdk.Stack {
           "ssm:GetParameter*",
           "events:PutEvents",
           "s3:GetObject",
+          "states:StartExecution",
+          "states:StopExecution",
+          "states:DescribeExecution",
         ],
         resources: [
           `arn:aws:dynamodb:*:${this.account}:table/Invoices`,
           `arn:aws:ssm:*:${this.account}:parameter/invoices/config/*`,
           `arn:aws:events:*:${this.account}:event-bus/Invoices`,
-          `arn:aws:s3:::aws-functionless-invoices-${this.account}-${this.region}/*`
+          `arn:aws:s3:::aws-functionless-invoices-${this.account}-${this.region}/*`,
+          `arn:aws:states:*:${this.account}:stateMachine:ImportInvoices`,
+          `arn:aws:states:*:${this.account}:execution:ImportInvoices:*`,
         ],
       })
     );
 
     // EventBridge event rule to trigger state machine
     new events.Rule(this, 'ImportInvoicesRule', {
-      ruleName: 'ImportInvoices',
+      ruleName: 'StartImport',
       description: 'Start execution of state machine ImportInvoices',
       eventBus: bus,
       eventPattern: {
@@ -73,6 +78,7 @@ export class ImportInvoicesStack extends cdk.Stack {
     new events.Rule(this, 'NewInvoiceRule', {
       ruleName: 'NewInvoiceImported',
       description: 'Send events to event bus in secondary region',
+      eventBus: bus,
       eventPattern: {
         source: [ "ImportInvoices" ],
         detailType: [ "NewInvoiceImported" ]
